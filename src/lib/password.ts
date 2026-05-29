4acc1fcebf0bc3d675171b7a129f829d59aff09a
nitinsutar <nitins.procam@gmail.com>
2026-05-29 19:13:02 +0530

    Add password generation helper

---
+++ src/lib/password.ts
@@ -0,0 +1,4 @@
+export function generatePassword(length = 12) {
+  const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789!@#$";
+  return Array.from({ length }, () => alphabet[Math.floor(Math.random() * alphabet.length)]).join("");
+}
